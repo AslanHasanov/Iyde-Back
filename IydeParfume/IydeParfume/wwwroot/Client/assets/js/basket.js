e6df79765cbe561317ecb0bce19523cbb0f1c802
AslanHasanov <aslanrh@code.edu.az>
2023-04-05 04:59:05 +0400

    Basket Ajax completed

--- IydeParfume/IydeParfume/wwwroot/Client/assets/js/basket.js
+++ IydeParfume/IydeParfume/wwwroot/Client/assets/js/basket.js
@@ -1,193 +1,4 @@
-﻿//$(document).on("click", ".add-product-to-basket-single-btn", function (e) {
-//    e.preventDefault();
-
-
-//    let aHref = e.target.href;
-//    let size = e.target.parentElement.parentElement.firstElementChild.children[0];
-
-//    let SizeId = size.value;
-
-//    let quantity = e.target.previousElementSibling.children[1];
-//    let Quantity = quantity.value;
-//    console.log(Quantity)
-
-//    $.ajax(
-//        {
-//            type: "POST",
-//            url: aHref,
-//            data: {
-//                SizeId: SizeId,
-//                Quantity: Quantity
-//            },
-//            error: function (err) {
-//                $(".product-details").html(err.responseText);
-
-//            }
-
-//        });
-
-
-
-//})
-
-//$(document).on("click", ".add-product-to-basket-single-btn", function (e) {
-//    e.preventDefault();
-
-//    document.getElementById('toaster').style.opacity = '1'
-//    setTimeout(() => {
-//        document.getElementById('toaster').style.opacity = '0'
-//    }, 2000);
-
-//    let aHref = e.target.href;
-//    let size = e.target.parentElement.parentElement.firstElementChild.children[0];
-
-//    let SizeId = size.value;
-
-//    let quantity = e.target.previousElementSibling.children[1];
-//    let Quantity = quantity.value;
-//    console.log(quantity)
-
-//    $.ajax(
-//        {
-//            type: "POST",
-//            url: aHref,
-//            data: {
-//                SizeId: SizeId,
-//                Quantity: Quantity
-//            },
-
-//            error: function (err) {
-//                $(".product-details-modal").html(err.responseText);
-
-//            }
-
-//        });
-
-//})
-
-//let btns = document.querySelectorAll(".add-to-basket")
-
-//btns.forEach(x => x.addEventListener("click", function (e) {
-//    e.preventDefault()
-
-//    let aHref = e.target.parentElement.href;
-
-
-//    document.getElementById('toaster').style.opacity = '1'
-//    setTimeout(() => {
-//        document.getElementById('toaster').style.opacity = '0'
-//    }, 1000);
-
-//    $.ajax(
-//        {
-//            type: "POST",
-//            url: aHref,
-
-//            error: function (err) {
-//                $(".product-details-modal").html(err.responseText);
-
-//            }
-
-//        });
-
-//}))
-
-
-//$(document).on("click", ".plus-btn", function (e) {
-//    e.preventDefault();
-
-//    var aHref = e.target.href;
-
-//    let size = e.target.nextElementSibling;
-//    console.log(size);
-//    let SizeId = size.value;
-
-//    $.ajax(
-//        {
-//            type: "POST",
-//            url: aHref,
-//            data: {
-//                SizeId: SizeId,
-//            },
-//            success: function (response) {
-
-//                $('.basket-block').html(response);
-
-//                $.ajax(
-//                    {
-//                        type: "GET",
-//                        url: updateHref,
-//                        success: function (response) {
-
-//                            $('.cart-block').html(response);
-
-
-//                        },
-//                        error: function (err) {
-//                            $(".product-details-modal").html(err.responseText);
-
-//                        }
-
-//                    });
-
-//            },
-//            error: function (err) {
-//                $(".product-details-modal").html(err.responseText);
-
-//            }
-
-//        });
-
-//})
-
-
-$(document).on("click", ".minus-btn", function (e) {
-    e.preventDefault();
-
-    var aHref = e.target.href;
-
-    let size = e.target.parentElement.children[3];
-    let SizeId = size.value;
-    console.log(SizeId)
-
-    let updateHref = e.target.nextElementSibling.nextElementSibling.nextElementSibling.href;
-    //$.ajax(
-    //    {
-    //        type: "GET",
-    //        url: aHref,
-    //        data: {
-    //            SizeId: SizeId,
-    //        },
-    //        success: function (response) {
-
-    //            $('.basket-block').html(response);
-
-    //            $.ajax(
-    //                {
-    //                    type: "GET",
-    //                    url: updateHref,
-    //                    success: function (response) {
-
-    //                        $('.cart-block').html(response);
-
-
-    //                    },
-    //                    error: function (err) {
-    //                        $(".product-details-modal").html(err.responseText);
-
-    //                    }
-
-    //                });
-
-    //        },
-    //        error: function (err) {
-    //            $(".product-details-modal").html(err.responseText);
-
-    //        }
-
-    //    });
-
-})
+﻿
 
 
 $(document).on("click", ".add-product-to-basket-single-btn", function (e) {
@@ -195,15 +6,8 @@ $(document).on("click", ".add-product-to-basket-single-btn", function (e) {
 
 
     let aHref = e.target.href;
-    let size = e.target.parentElement.parentElement.firstElementChild.children[1];
+    let size = e.target.parentElement.previousElementSibling.children[2];
     let SizeId = size.value;
-    console.log(SizeId);
-
-
-
-    let quantity = e.target.previousElementSibling.children[1];
-    let Quantity = quantity.value;
-    console.log(Quantity)
 
     $.ajax(
         {
@@ -211,7 +15,6 @@ $(document).on("click", ".add-product-to-basket-single-btn", function (e) {
             url: aHref,
             data: {
                 SizeId: SizeId,
-                Quantity: Quantity
             },
             success: function (response) {
 
@@ -229,32 +32,57 @@ $(document).on("click", ".add-product-to-basket-single-btn", function (e) {
 
 $(document).on("click", ".plus-btn", function (e) {
     e.preventDefault();
+    console.log(e.target)
 
     fetch(e.target.href)
         .then(response => response.text())
         .then(data => {
-            $('.cartPageJs').html(data);
+            $('.right-card').html(data);
 
-            fetch(e.target.nextElementSibling.href)
-                .then(response => response.text())
-                .then(data => {
-                    $('.quantity-display').html(data);
-                })
+           
         })
 })
 
 $(document).on("click", ".minus-btn", function (e) {
     e.preventDefault();
 
-    fetch(e.target.href)
-        .then(response => response.text())
-        .then(data => {
-            $('.cartPageJs').html(data);
+    var aHref = e.target.href;
+    let size = e.target.nextElementSibling.nextElementSibling.nextElementSibling;
+    let SizeId = size.value;
 
-            fetch(e.target.nextElementSibling.href)
-                .then(response => response.text())
-                .then(data => {
-                    $('.quantity-display').html(data);
-                })
-        })
+    $.ajax(
+        {
+            type: "GET",
+            url: aHref,
+            data: {
+                SizeId: SizeId,
+            },
+            success: function (response) {
+
+                $('.cartPageJs').html(response);
+
+            }
+
+        });
+})
+$(document).on("click", ".plus-btn", function (e) {
+    e.preventDefault();
+
+    var aHref = e.target.href;
+    let size = e.target.nextElementSibling;
+    let SizeId = size.value;
+
+    $.ajax(
+        {
+            type: "POST",
+            url: aHref,
+            data: {
+                SizeId: SizeId,
+            },
+            success: function (response) {
+
+                $('.cartPageJs').html(response);
+            }
+        });
+
 })
